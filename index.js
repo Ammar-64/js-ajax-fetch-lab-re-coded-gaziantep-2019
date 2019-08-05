26f6769f5f85866125c8bc19848b57b4cecb2c2a
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-08-05 00:20:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const baseURL = 'https://api.github.com/';
 const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
 const user_repo = 'ammar-64/js-ajax-fetch-lab';
+const tokens = getToken();
 function getToken() {
+  const token = '6c65fa3591ae36aa9e8f1af7cf31b2e5a5e1b47a'
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '6c65fa3591ae36aa9e8f1af7cf31b2e5a5e1b47a';
+  return token;
 }
-const token = getToken();
 function forkRepo() {
   fetch(`https://api.github.com/repos/${repo}/forks`,{
     headers: {
-    Authorization: `token ${token}`;
+    Authorization: `token ${tokens}`;
   }
 })
-  .then(res => res.json())
+  .then(res => response.json())
   .then(json => console.log(res););
   })
   //use fetch to fork it!
